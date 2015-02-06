37000c72464582a437f0e102ead29c3444dae560
Eric Murphy <murph141@purdue.edu>
2015-02-06 13:40:48 -0500

    Testing

--- Mine.lua
+++ Mine.lua
@@ -1,3 +1,58 @@
 scriptId = 'com.eric.mine.craft'
 scriptTitle = "Hopefully this works"
 scriptDetailsUrl = ""
+
+function activeAppName()
+
+  return(activeApp)
+
+end
+
+
+function onForegroundWindowChange(app, title)
+
+  if(app == "JAVAW.EXE") then
+    myo.debug("Correct Window")
+    return true
+  else
+    return false
+  end
+
+end
+
+
+function onActiveChange(isActive)
+
+  if(isActive) then
+    myo.debug("active")
+  else
+    myo.debug("inactive")
+  end
+
+end
+
+function mousePoseControl(pose, edge)
+
+  if(pose == "") then
+  end
+
+end
+
+function onPoseEdge(pose, edge)
+
+  myo.debug("pose:" .. pose)
+  myo.debug("edge:" .. edge)
+
+  mousePoseControl(pose, edge)
+
+end
+
+function onPeriodic()
+
+  if(centerMode) then
+    myo.centerMousePosition()
+  end
+
+end
+
+myo.controlMouse(true)
